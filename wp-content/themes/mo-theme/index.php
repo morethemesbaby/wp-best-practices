804f417576ae62d5dce1123c04c792fc956f6e9d
metamn <bartus.csongor@gmail.com>
2018-09-26 15:10:09 +0300

    Mo Theme added

---
+++ wp-content/themes/mo-theme/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The main template file.
+ *
+ * It is required by WordPress. Currently is empty.
+ *
+ * @link https://developer.wordpress.org/themes/getting-started/what-is-a-theme/
+ *
+ * @package MoTheme
+ * @since 1.0.0.
+ */
+?>
