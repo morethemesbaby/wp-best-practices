360ca300335f9def1d49e998dd010ac9720df9aa
metamn <bartus.csongor@gmail.com>
2018-10-01 09:57:53 +0300

    Working on the Home template

--- wp-content/themes/mo-theme/home.php
+++ wp-content/themes/mo-theme/home.php
@@ -16,9 +16,9 @@ get_header();
 $component = new MoThemeHTMLComponent();
 ?>
 
-<section <?php $component->attributes->display( array( 'block' => 'home' ) ); ?>>
+<section <?php apply_filters( 'mo_theme_home_attributes', $component->attributes->display( array( 'block' => 'home' ) ) ); ?>>
 	<?php
-		$component->title->display( array( 'title' => 'Home' ) );
+		apply_filters( 'mo_theme_home_title', $component->title->display( array( 'title' => 'Home' ) ) );
 		get_template_part( 'template-parts/post-list/post-list', '' );
 	?>
 </section>
