9b380068c49e7dc0c1f0c1114cbdee828953a301
metamn <bartus.csongor@gmail.com>
2018-10-09 19:13:44 +0300

    Working on the plugin setup

--- wp-content/plugins/mo-plugin/includes/class-moplugin-setup.php
+++ wp-content/plugins/mo-plugin/includes/class-moplugin-setup.php
@@ -21,7 +21,19 @@ if ( ! class_exists( 'MoPluginSetup' ) ) {
 		 *
 		 * @var array $arguments An Array of arguments.
 		 */
-		public $arguments = array();
+		public $arguments = array(
+			'has_admin_interface'    => false,
+			'has_public_interface'   => false,
+			'admin_assets_folder'    => 'admin',
+			'public_assets_folder'   => 'public',
+			'js_folder'              => 'js',
+			'css_folder'             => 'css',
+			'images_folder'          => 'images',
+			'javascript_file_handle' => 'script',
+			'css_file_handle'        => 'style',
+			'javascript_extension'   => 'js',
+			'plugin_dir_url'         => plugin_dir_url( __FILE__ )
+		);
 
 		/**
 		 * Sets up the class.
@@ -34,8 +46,8 @@ if ( ! class_exists( 'MoPluginSetup' ) ) {
 		public function __construct( $arguments ) {
 			$this->arguments = $this->array_merge( $this->arguments, $arguments );
 
-			add_action( 'after_setup_plugin', array( $this, 'setup_variables' ) );
-			add_action( 'after_setup_plugin', array( $this, 'setup_functionalities' ) );
+			add_action( 'plugins_loaded', array( $this, 'setup_variables' ) );
+			add_action( 'plugins_loaded', array( $this, 'setup_functionalities' ) );
 			add_action( 'wp_enqueue_scripts', array( $this, 'add_styles' ) );
 			add_action( 'wp_enqueue_scripts', array( $this, 'add_scripts' ) );
 		}
@@ -48,31 +60,19 @@ if ( ! class_exists( 'MoPluginSetup' ) ) {
 		 * @return void
 		 */
 		public function setup_variables() {
-			$plugin = wp_get_plugin();
+			$plugin = wp_get_data();
 
 			$this->name        = $plugin->get( 'Name' );
 			$this->version     = $plugin->get( 'Version' );
 			$this->text_domain = $plugin->get( 'TextDomain' );
 
-			$this->assets_folder = $this->arguments['assets_folder'];
-			$this->plugin_path    = get_template_directory() . '/';
-			$this->include_path  = $this->plugin_path . $this->arguments['include_folder'];
-
-			$this->functionality_set = $this->arguments['functionality_set'];
-			$this->customization_set = $this->arguments['customization_set'];
-
-			$this->javascript_file_name     = $this->arguments['javascript_folder'] . $this->text_domain . $this->arguments['javascript_extension'];
-			$this->javascript_file_location = get_plugin_file_uri( '/' . $this->assets_folder );
-			$this->javascript_file_handle   = $this->text_domain . $this->arguments['javascript_file_handle'];
-			$this->javascript_src           = $this->javascript_file_location . $this->javascript_file_name;
-			$this->javascript_deps          = array();
-			$this->javascript_in_footer     = true;
-			$this->javascript_timestamp     = $this->version;
-
-			$this->css_file_handle = $this->text_domain . $this->arguments['css_file_handle'];
-			$this->css_src         = get_stylesheet_uri();
-			$this->css_deps        = array();
-			$this->css_timestamp   = $this->version;
+			$this->has_admin_interface  = $this->arguments['has_admin_interface'];
+			$this->has_public_interface = $this->arguments['has_public_interface'];
+			$this->admin_assets_folder  = $this->arguments['admin_assets_folder'];
+			$this->public_assets_folder = $this->arguments['public_assets_folder'];
+			$this->js_folder            = $this->arguments['js_folder'];
+			$this->css_folder           = $this->arguments['css_folder'];
+			$this->images_folder        = $this->arguments['images_folder'];
 		}
 
 		/**
@@ -83,11 +83,45 @@ if ( ! class_exists( 'MoPluginSetup' ) ) {
 		 * @return void
 		 */
 		public function setup_functionalities() {
-			$functionalities = new MoPluginFunctionalities(
-				array(
-					'set' => $this->functionality_set,
-				)
-			);
+			if ( true === $this->has_admin_interface ) {
+				$this->setup_admin_functionalities();
+			}
+
+			if ( true === $this->has_public_interface ) {
+				$this->setup_public_functionalities();
+			}
+		}
+
+		/**
+		 * Sets up admin functionalities.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @return void
+		 */
+		public function setup_admin_functionalities() {
+			$this->setup_scripts(array(
+				'folder' => $this->admin_assets_folder,
+			));
+		}
+
+		/**
+		 * Sets up arguments for 'wp_enqueue_style`
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $arguments The arguments array.
+		 * @return void
+		 */
+		public function setup_scripts( $arguments = array() ) {
+			$folder = $arguments['folder'];
+
+			$javascript_file_name    = "{$this->text_domain}-{$this->folder}.{$this->javascript_extension}";
+			$javascript_file_handle  = "{$this->text_domain}-{$this->javascript_file_handle}";
+			$javascript_src          = "{$this->plugin_dir_url}/{$this->javascript_file_name}";
+			$javascript_dependencies = $this->javascript_dependencies;
+			$javascript_in_footer    = $this->javascript_in_footer;
+			$javascript_timestamp    = $this->version;
 		}
 
 
