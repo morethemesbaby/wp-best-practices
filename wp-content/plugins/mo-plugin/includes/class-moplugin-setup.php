104eb9c993f5efe3ff50213c7acc06e200808005
metamn <bartus.csongor@gmail.com>
2018-10-12 09:38:21 +0300

    Plugin assets refactored

--- wp-content/plugins/mo-plugin/includes/class-moplugin-setup.php
+++ wp-content/plugins/mo-plugin/includes/class-moplugin-setup.php
@@ -15,7 +15,7 @@ if ( ! class_exists( 'MoPluginSetup' ) ) {
 	class MoPluginSetup extends MoPluginBase {
 
 		/**
-		 * Theme arguments.
+		 * Class arguments.
 		 *
 		 * @since 1.0.0
 		 *
@@ -38,49 +38,6 @@ if ( ! class_exists( 'MoPluginSetup' ) ) {
 			'css_dependencies'        => array(),
 			'theme_feature_set'       => '',
 			'theme_features'          => array(),
-		);
-
-		/**
-		 * Arguments for a theme asset.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @var array
-		 */
-		public $asset_arguments = array(
-			'folder' => '',
-			'action' => '',
-		);
-
-		/**
-		 * Arguments to set up enqueue for an asset.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @var array
-		 */
-		public $enqueue_arguments = array(
-			'folder'       => '',
-			'extension'    => '',
-			'file_handle'  => '',
-			'subfolder'    => '',
-			'dependencies' => array(),
-			'in_footer'    => false,
-		);
-
-		/**
-		 * Arguments to enqueue a script.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @var array
-		 */
-		public $enqueue_script_arguments = array(
-			'file_handle'  => '',
-			'src'          => '',
-			'dependencies' => array(),
-			'timestamp'    => '',
-			'in_footer'    => false,
 		);
 
 		/**
@@ -233,20 +190,10 @@ if ( ! class_exists( 'MoPluginSetup' ) ) {
 			$this->version     = $plugin['Version'];
 			$this->text_domain = $plugin['TextDomain'];
 
-			$this->has_admin_interface     = $this->arguments['has_admin_interface'];
-			$this->has_public_interface    = $this->arguments['has_public_interface'];
-			$this->admin_assets_folder     = $this->arguments['admin_assets_folder'];
-			$this->public_assets_folder    = $this->arguments['public_assets_folder'];
-			$this->javascript_folder       = $this->arguments['javascript_folder'];
-			$this->css_folder              = $this->arguments['css_folder'];
-			$this->images_folder           = $this->arguments['images_folder'];
-			$this->javascript_file_handle  = $this->arguments['javascript_file_handle'];
-			$this->css_file_handle         = $this->arguments['css_file_handle'];
-			$this->javascript_extension    = $this->arguments['javascript_extension'];
-			$this->javascript_dependencies = $this->arguments['javascript_dependencies'];
-			$this->javascript_in_footer    = $this->arguments['javascript_in_footer'];
-			$this->css_extension           = $this->arguments['css_extension'];
-			$this->css_dependencies        = $this->arguments['css_dependencies'];
+			$this->has_admin_interface  = $this->arguments['has_admin_interface'];
+			$this->has_public_interface = $this->arguments['has_public_interface'];
+			$this->admin_assets_folder  = $this->arguments['admin_assets_folder'];
+			$this->public_assets_folder = $this->arguments['public_assets_folder'];
 
 			$this->theme_feature_set = $this->arguments['theme_feature_set'];
 			$this->theme_features    = $this->arguments['theme_features'];
@@ -262,161 +209,35 @@ if ( ! class_exists( 'MoPluginSetup' ) ) {
 		 */
 		public function setup_assets() {
 			if ( true === $this->has_admin_interface ) {
-				$this->setup_asset(
+				$arguments = $this->array_merge(
+					$this->arguments,
 					array(
-						'folder' => $this->admin_assets_folder,
-						'action' => 'admin_enqueue_scripts',
+						'folder'       => $this->admin_assets_folder,
+						'admin_folder' => $this->admin_assets_folder,
+						'action'       => 'admin_enqueue_scripts',
+						'version'      => $this->version,
+						'text_domain'  => $this->text_domain,
 					)
 				);
+
+				$assets = new MoPluginAssets( $arguments );
+				$assets->add();
 			}
 
 			if ( true === $this->has_public_interface ) {
-				$this->setup_asset(
+				$arguments = $this->array_merge(
+					$this->arguments,
 					array(
-						'folder' => $this->public_assets_folder,
-						'action' => 'wp_enqueue_scripts',
+						'folder'       => $this->public_assets_folder,
+						'admin_folder' => $this->admin_assets_folder,
+						'action'       => 'wp_enqueue_scripts',
+						'version'      => $this->version,
+						'text_domain'  => $this->text_domain,
 					)
 				);
-			}
-		}
 
-		/**
-		 * Sets up a specific asset.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @param array $arguments The arguments array.
-		 * @return void
-		 */
-		public function setup_asset( $arguments ) {
-			$arguments = $this->array_merge( $this->asset_arguments, $arguments );
-
-			$script = $this->setup_enqueue(
-				array(
-					'folder'       => $arguments['folder'],
-					'extension'    => $this->javascript_extension,
-					'file_handle'  => $this->javascript_file_handle,
-					'subfolder'    => $this->javascript_folder,
-					'dependencies' => $this->javascript_dependencies,
-					'in_footer'    => $this->in_footer( $arguments ),
-				)
-			);
-
-			$style = $this->setup_enqueue(
-				array(
-					'folder'       => $arguments['folder'],
-					'extension'    => $this->css_extension,
-					'file_handle'  => $this->css_file_handle,
-					'subfolder'    => $this->css_folder,
-					'dependencies' => $this->css_dependencies,
-				)
-			);
-
-			add_action(
-				$arguments['action'],
-				function() use ( $script ) {
-					$this->add_script( $script );
-				}
-			);
-
-			add_action(
-				$arguments['action'],
-				function() use ( $style ) {
-					$this->add_style( $style );
-				}
-			);
-		}
-
-		/**
-		 * Sets up arguments for enqueue scripts or styles.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @param array $arguments The arguments array.
-		 * @return array
-		 */
-		public function setup_enqueue( $arguments ) {
-			$arguments = $this->array_merge( $this->enqueue_arguments, $arguments );
-
-			$folder    = $arguments['folder'];
-			$file_name = "{$this->text_domain}-{$folder}.{$arguments['extension']}";
-
-			return array(
-				'file_handle'  => "{$this->text_domain}-{$arguments['file_handle']}",
-				'src'          => PLUGIN_DIR_URL . "{$folder}/{$arguments['subfolder']}/{$file_name}",
-				'dependencies' => $arguments['dependencies'],
-				'timestamp'    => $this->version,
-				'in_footer'    => $arguments['in_footer'],
-			);
-		}
-
-
-		/**
-		 * Enqueues a script.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @param array $arguments The arguments array.
-		 * @return void
-		 */
-		public function add_script( $arguments = array() ) {
-			$arguments = $this->array_merge( $this->enqueue_script_arguments, $arguments );
-
-			if ( '' !== $arguments['src'] ) {
-				wp_enqueue_script(
-					$arguments['file_handle'],
-					$arguments['src'],
-					$arguments['dependencies'],
-					$arguments['timestamp'],
-					$arguments['in_footer']
-				);
-			}
-		}
-
-		/**
-		 * Enqueues a style.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @param array $arguments The arguments array.
-		 * @return void
-		 */
-		public function add_style( $arguments = array() ) {
-			$arguments = $this->array_merge( $this->enqueue_script_arguments, $arguments );
-
-			if ( '' !== $arguments['src'] ) {
-				wp_enqueue_style(
-					$arguments['file_handle'],
-					$arguments['src'],
-					$arguments['dependencies'],
-					$arguments['timestamp']
-				);
-			}
-		}
-
-		/**
-		 * Decides if a script can be / must be loaded in the footer.
-		 *
-		 * On the admin scripts cannot be loaded in the footer.
-		 * On public they can be loaded anywhere.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @param array $arguments The arguments array.
-		 * @return boolean
-		 */
-		public function in_footer( $arguments = array() ) {
-			$arguments = $this->array_merge(
-				array(
-					'folder' => '',
-				),
-				$arguments
-			);
-
-			if ( $arguments['folder'] === $this->admin_assets_folder ) {
-				return false;
-			} else {
-				return $this->javascript_in_footer;
+				$assets = new MoPluginAssets( $arguments );
+				$assets->add();
 			}
 		}
 	}
